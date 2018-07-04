42628cf0c1a4255d015ab571f405db62febd8bb4
Joanna Gora <joanna.gora@protonmail.com>
2018-07-04 12:35:12 +0100

    passed buy_drink_more_drunk test

--- customer.rb
+++ customer.rb
@@ -1,13 +1,18 @@
+require_relative("drink")
 class Customer
 
-  attr_reader :name, :age, :money_amount
+  attr_reader :name, :age, :money_amount, :drunkenness_level
 
   def initialize(name, age, money_amount)
     @name = name
     @age = age
     @money_amount = money_amount
-    @drunkeness_level = 0
+    @drunkenness_level = 0
   end
 
+  def buy_drink(drink)
+    @money_amount -= drink.price
+    @drunkenness_level += drink.alcohol_level
+  end
 
 end
--- specs/customer_spec.rb
+++ specs/customer_spec.rb
@@ -4,6 +4,7 @@ require_relative("../customer")
 class TestCustomer < MiniTest::Test
   def setup
     @mike_customer = Customer.new("Mike", 82, 100)
+    @budweisser_drink = Drink.new("Budweisser", "beer", 2, 5)
   end
 
   def test_how_much_money
@@ -12,6 +13,24 @@ class TestCustomer < MiniTest::Test
     assert_equal(expected, actual)
   end
 
+  def test_drunkenness
+    expected = 0
+    actual = @mike_customer.drunkenness_level
+    assert_equal(expected, actual)
+  end
 
+  def test_buy_drink__less_money
+    @mike_customer.buy_drink(@budweisser_drink)
+    expected = 98
+    actual = @mike_customer.money_amount
+    assert_equal(expected, actual)
+  end
+
+  def test_buy_drink__more_drunk
+    @mike_customer.buy_drink(@budweisser_drink)
+    expected = 5
+    actual = @mike_customer.drunkenness_level
+    assert_equal(expected, actual)
+  end
 
 end
