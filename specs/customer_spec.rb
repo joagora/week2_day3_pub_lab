0f53a1d6e96806748a64cd8e05c73d6241ecc08a
Joanna Gora <joanna.gora@protonmail.com>
2018-07-04 11:20:20 +0100

    prepared the minitest

---
+++ specs/customer_spec.rb
@@ -0,0 +1,9 @@
+require("minitest/autorun")
+require_relative("../customer")
+
+class TestCustomer < MiniTest::Test
+  def setup
+
+  end
+
+end
---
+++ specs/pub_spec.rb
@@ -0,0 +1,9 @@
+require("minitest/autorun")
+require_relative("../pub")
+
+class TestPub < MiniTest::Test
+  def setup
+
+  end
+
+end
